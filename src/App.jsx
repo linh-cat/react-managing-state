82a9b83992c7d1790688f1ef7ede0825d29b6b64
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2020-10-06 22:12:21 +0700

    managing react state

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Products from "./Products";
+import Detail from "./Detail";
+import Cart from "./Cart";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<h1>Welcom To Carved Rock Fitness</h1>} />
+            <Route path="/:category" element={<Products />} />
+            <Route path="/category/:id" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
